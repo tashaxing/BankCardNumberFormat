ae1ba6f14ed0d1221ef7dc8116b3f045e9aa0c47
Ethan He <tashaxing123@163.com>
2016-06-08 17:53:15 +0800

    An iOS  textfield demo to format banknumber
    automatically format the bank number every 4 digits and limit the
    digits number

---
+++ BankCardNumberFormat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BankCardNumberFormat
+//
+//  Created by yxhe on 16/5/4.
+//  Copyright © 2016年 yxhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BankCardNumberFormat/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  BankCardNumberFormat
+//
+//  Created by yxhe on 16/5/4.
+//  Copyright © 2016年 yxhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController<UITextFieldDelegate>
+
+@end
+
